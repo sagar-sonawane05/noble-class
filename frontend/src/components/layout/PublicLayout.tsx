67609204318df00116150ea666f082049996520c
CodeWithSagarMali <sagarsonwane90112@gmail.com>
2026-07-17 10:39:47 +0530

    whatsapp button deleted

--- frontend/src/components/layout/PublicLayout.tsx
+++ frontend/src/components/layout/PublicLayout.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../common/Navbar';
 import Footer from '../common/Footer';
-import BackToTop from '../common/BackToTop';
 
 export const PublicLayout: React.FC = () => {
   return (
@@ -12,7 +11,6 @@ export const PublicLayout: React.FC = () => {
         <Outlet />
       </main>
       <Footer />
-      <BackToTop />
     </div>
   );
 };
